0a2981becebb120544a6658ddc344ce488c367fe
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-02-13 18:08:22 +0900

    코딩애플 | 반복문] vue로 만드는 직방 웹앱
    vue 반복문 문법
    v-for="이름 in 횟수(Array 이름)" :key="이름"
    
    이때 가장 처음으로 나온 in 왼쪽의 '이름'은 2개를 가질 수 있는데 편의상 좌변수, 우변수라고 명칭할 때
    좌변수는 뒤의 Array의 아이템 개수를 한 번씩 반복하거나 횟수만큼 반복하는 변수를 의미.
    우변수는 0,1,2와 같이 1씩 증가하는 정수를 의미한다.
    
    만일 in의 왼쪽의 '이름'이 2개가 된다면 통상적으로 (a, i)라는 문자가 주로 사용되고 뒤의 :key="i" 라고 적는다. 
    왜냐하면 :key라는 속성은 반복되어 생긴 엘리먼트들을 구분하기 위한 속성이기 때문이다. 앞서 말한 우변수와 관련이 있는 속성같다.
    
    반복문의 속성 중 i라는 1씩 증가하는 정수 속성이 Array의 아이템의 n번째 접근에 용이하게 작용할 것 같다.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="menu">
+    <!-- <a v-for="(a, i) in 메뉴들" :key="i">{{ i }}</a> -->
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  </div>
+
+  <div v-for="(a, i) in 3" :key="i">
+    <h4>{{ products[i] }}</h4>
+    <p>{{ price[i] }}만원</p>
+  </div>
   
-  <div>
-    <h4>{{products[0]}}</h4>
-    <p>{{ price1 }} 만원 <!-- {{ }} 중괄호 두 개 문법 - vue의 특징2 (중괄호 2개에 오브젝트에서 만들었던 데이터이름을 집어넣으면 됨) --></p>
+    <!-- 혹은 -->
+  <div v-for="(a, i) in products" :key="i">
+    <h4>{{ a }}</h4>
+    <p>{{ price[i] }}만원</p>
   </div>
-
-  <div>
-    <h4>{{products[1]}}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
-
-  <div>
-    <h4>{{products[2]}}</h4>
-    <p>{{ price2 }} 만원</p>
-  </div>
+  
 </template>
 
 <script>
@@ -23,8 +23,8 @@ export default {
   name: 'App',
   data() { /* 데이터 보관함 - vue의 특징1 (데이터는 오브젝트 형태로 저장)*/
     return {
-      price1 : 60,
-      price2 : 70,
+      price : [60, 70, 80],
+      메뉴들 : ["Home", "Shop", "About"],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
@@ -40,6 +40,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: #fff;
+  padding: 10px;
+}
+
 </style>
