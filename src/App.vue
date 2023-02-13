61b5f94b5005b1227af8fc35d53be2b543780bbf
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-02-13 16:00:26 +0900

    코딩애플] vue로 만드는 직방 웹앱
    vue를 처음 공부하다 보니 깃허브 업로드도 조금 어렵지만 열심히 푸딩!! 이다!!!

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>{{ price1 }} 만원 <!-- {{ }} 중괄호 두 개 문법 - vue의 특징2 (중괄호 2개에 오브젝트에서 만들었던 데이터이름을 집어넣으면 됨) --></p>
+  </div>
+
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() { /* 데이터 보관함 - vue의 특징1 (데이터는 오브젝트 형태로 저장)*/
+    return {
+      price1 : 60,
+      price2 : 70,
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
