f79290fa27ac94e0deab696ebd8f4f156b602d5a
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-02-13 19:15:32 +0900

    코딩애플 | 클릭 이벤트와 함수] vue로 만드는 직방 웹앱
    클릭 이벤트를 감지할 때
    js에서는 onClick=""을 사용했다면
    vue에서는 v-on:click=""나 v-on을 축약한 @click=""을 사용한다. vue문법이라고 외워도 좋다.
    클릭이벤트 외에도 다양한 속성(마우스 오버 등)이 있으니 ctrl + space 바를 눌러보면 좋다.
     
    함수는 데이터보관함이 끝나는 컴마 밑에 줄에 쓴다고 한다. (왜 그런지는 아직 잘 모르겠다)
    함수를 만들 때에는
    methods : {
         함수이름() {
             },
        함수이름1() {
            }
       },
    의 형태로 쓰면 된다. 함수가 하나일 경우에는 컴마를 붙이지 않고, 두 개 이상부터는 마지막 함수의 전 함수까지는 컴마를 쓴다. 
    마치 오브젝트 같다.
    
    그리고 세 div의 클릭이벤트는 내가 푼 숙제의 답!

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,24 @@
     <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
   </div>
 
-  <div v-for="(a, i) in 3" :key="i">
-    <h4>{{ products[i] }}</h4>
-    <p>{{ price[i] }}만원</p>
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>{{ price1 }} 만원</p>
+    <button @click="신고수[0]++">허위매물신고</button>  <span>신고 수 : {{신고수[0]}}</span>
   </div>
-  
-    <!-- 혹은 -->
-  <div v-for="(a, i) in products" :key="i">
-    <h4>{{ a }}</h4>
-    <p>{{ price[i] }}만원</p>
+
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>{{ price2 }} 만원</p>
+    <button @click="신고수[1]++">허위매물신고</button>  <span>신고 수 : {{신고수[1]}}</span>
   </div>
-  
+
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>{{ price2 }} 만원</p>
+    <button @click="신고수[2]++">허위매물신고</button>  <span>신고 수 : {{신고수[2]}}</span>
+  </div>
+
 </template>
 
 <script>
@@ -23,11 +30,19 @@ export default {
   name: 'App',
   data() { /* 데이터 보관함 - vue의 특징1 (데이터는 오브젝트 형태로 저장)*/
     return {
-      price : [60, 70, 80],
+      신고수 : [0, 0, 0],
+      price1 : 60,
+      price2 : 70,
       메뉴들 : ["Home", "Shop", "About"],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
   },
+  methods : {
+    increase(){
+      this.신고수 += 1;
+    }
+  },
+
   components: {
   }
 }
