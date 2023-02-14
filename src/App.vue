0f494ccbd23cfb3dd69a77d78484d7a48ed866ed
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2023-02-14 18:34:58 +0900

    코딩애플 | 모달창 만들기] vue로 만드는 직방 웹앱
    모달창과 같이 다양한 이벤트를 만들 때 팁
    1. html과 css로 해당 이벤트의 시각적 레이아웃을 먼저 구현한다.
    2. 구현한 레이아웃들을 데이터 보관함의 데이터로 저장한다.
    3. 보관한 데이터를 상호작용이 가능하도록 html의 해당 태그에 vue문법으로 작성한다.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,61 @@
 <template>
+
+<div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
+
+
+
+
   <div class="menu">
     <!-- <a v-for="(a, i) in 메뉴들" :key="i">{{ i }}</a> -->
     <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
   </div>
 
-  <div>
-    <h4>{{products[0]}}</h4>
-    <p>{{ price1 }} 만원</p>
-    <button @click="신고수[0]++">허위매물신고</button>  <span>신고 수 : {{신고수[0]}}</span>
-  </div>
+
 
   <div>
+    <img src="./assets/room0.jpg" class="room-img"/>
+    <h4  @click="모달창열렸니 = true">{{products[0]}}</h4>
+    <p>{{ price1 }} 만원</p>
+    <button @click="신고수[0]++">허위매물신고</button>
+    <span>신고 수 : {{신고수[0]}}</span>
+  </div>
+
+
+
+  <div>
+    <img src="./assets/room1.jpg" class="room-img"/>
     <h4>{{products[1]}}</h4>
     <p>{{ price2 }} 만원</p>
-    <button @click="신고수[1]++">허위매물신고</button>  <span>신고 수 : {{신고수[1]}}</span>
+    <button @click="신고수[1]++">허위매물신고</button>
+    <span>신고 수 : {{신고수[1]}}</span>
   </div>
 
   <div>
+    <img src="./assets/room2.jpg" class="room-img"/>
     <h4>{{products[2]}}</h4>
     <p>{{ price2 }} 만원</p>
-    <button @click="신고수[2]++">허위매물신고</button>  <span>신고 수 : {{신고수[2]}}</span>
+    <button @click="신고수[2]++">허위매물신고</button>
+    <span>신고 수 : {{신고수[2]}}</span>
   </div>
 
 </template>
 
 <script>
+// import 
 
 export default {
+  
   name: 'App',
   data() { /* 데이터 보관함 - vue의 특징1 (데이터는 오브젝트 형태로 저장)*/
     return {
+      모달창열렸니 : false, /* 리액트에서는 이러한 데이터 보관함을 state라고 한다. true(혹은 0)가 열린상태, false(혹은 1)가 닫힌 상태.  */
       신고수 : [0, 0, 0],
       price1 : 60,
       price2 : 70,
@@ -49,6 +75,34 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: #fff;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
+
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
